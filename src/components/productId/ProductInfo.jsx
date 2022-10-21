b806998087c9b035f68a06eb82ed3a01b02bb938
RosslynAsto <rosly_13_5@hotmail.com>
2022-10-21 11:22:50 -0500

    21-10v1

---
+++ src/components/productId/ProductInfo.jsx
@@ -0,0 +1,42 @@
+
+import React, { useState } from 'react'
+
+const ProductInfo = ({product}) => {
+    const [counter, setCounter] = useState(1)
+    const handleMinus = () =>{
+        if (counter - 1 > 0) {
+            setCounter(counter-1)
+        }
+       
+    }
+    const handlePlus = () => {
+        setCounter(counter + 1)
+    }
+  return (
+    <article className='product-info'>
+        <h2 className='product-info__title'>{product?.title}</h2>
+        <p className='product-info__description'>{product?.description}</p>
+        <footer className='product-info__footer'>
+            <div className='product-info__price-container'>
+                <h3 className='product-info__price-label'>Price</h3>
+                <p className='product-info__price-number'>{product?.price}</p>
+            </div>
+            <div className='product-info__quantity-container'>
+                <h3 className='product-info__quantity-label'>Quantity</h3>
+                <div className='counter'>
+                    <div onClick={handleMinus} className='counter__minus'>-</div>
+                    <div className='counter__number'>{counter}</div>
+                    <div onClick={handlePlus} className='counter__plus'>+</div>
+                </div>
+               
+            </div>
+            <button className='product-info__btn'>Add to card <i
+             className='product__icon fa-solid fa-cart-shopping'></i></button>
+            
+
+        </footer>
+    </article>
+  )
+}
+
+export default ProductInfo
--- src/pages/ProductId.jsx
+++ src/pages/ProductId.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import ProductInfo from '../components/productId/ProductInfo'
+import SimilarProducts from '../components/productId/SimilarProducts'
 
 const ProductId = () => {
-  const [product, setProduct] = useState(second)
-  const {id} = useParams()
+  
+const {id} = useParams()
 
+  const [product, setProduct] = useState()
   useEffect(() => {
     URL=`https://ecommerce-api-react.herokuapp.com/api/v1/products/${id}`
    axios.get(URL)
@@ -13,9 +16,19 @@ const ProductId = () => {
      .catch(err => console.log(err))
   }, [id])
   
+  const [categories, setCategories] = useState()
+  useEffect(() => {
+   const URL='https://ecommerce-api-react.herokuapp.com/api/v1/products/categories'
+   axios.get(URL)
+   .then(res => setCategories(res.data))
+   .catch(err => console.log(err))
+  }, [])
+  
+  console.log(product);
   return (
     <div>
-      <p>{id}</p>
+      <ProductInfo product={product} />
+      <SimilarProducts />
     </div>
   )
 }
