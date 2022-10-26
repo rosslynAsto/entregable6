79bf3c600d06d0c0ac3f184343427bb9e932968a
RosslynAsto <rosly_13_5@hotmail.com>
2022-10-25 19:25:36 -0500

    25-10v3

--- src/components/cart/CartProducts.jsx
+++ src/components/cart/CartProducts.jsx
@@ -1,14 +1,31 @@
+import axios from 'axios'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import getConfig from '../../utils/getConfig'
+import {getAllProductsCart} from '../../store/slices/cart.slice'
+import './styles/cartProducts.css'
 
 const CartProducts = ({product}) => {
+
+  const dispatch = useDispatch()
+ const handleDelete = () => {
+  const URL= `https://ecommerce-api-react.herokuapp.com/api/v1/cart/${product.id}`
+  axios.delete(URL, getConfig())
+    .then(res => {
+      console.log(res.data)
+      dispatch(getAllProductsCart())
+    })
+    . catch(err => console.log(err))
+ }
+
   return (
-    <article>
+    <article className='cart-p'>
         <h2>{product.title}</h2>
         <ul>
             <li><span>Price</span>{product.price}</li>
             <li><span>Quantity</span>{product.productsInCart.quantity}</li>
         </ul>
-       <button><i className="fa-regular fa-trash-can"></i></button>
+       <button onClick={handleDelete} className='card-p__btn'><i className=" cart-p__icon fa-regular fa-trash-can"></i></button>
     </article>
   )
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,9 @@
+import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import CartProducts from '../components/cart/CartProducts'
-import { getAllProductsCart } from '../store/slices/cart.slice'
+import { getAllProductsCart, setCartGlobal } from '../store/slices/cart.slice'
+import getConfig from '../utils/getConfig'
 
 const Cart = () => {
 
@@ -13,6 +15,22 @@ useEffect(() => {
 }, [])
 
 console.log(cart);
+const handlePurchases = ()=>{
+  const URL='https://ecommerce-api-react.herokuapp.com/api/v1/purchases'
+  const data = {
+    "street": "Green St. 1456",
+    "colony": "Southwest",
+    "zipCode": 12345,
+    "city": "USA",
+    "references": "Some references"
+  }
+  axios.post(URL, data, getConfig())
+  .then(res => {
+    console.log(res.data)
+    dispatch(setCartGlobal(null))
+  })
+  .catch(err => console.log(err))
+}
 
   return (
     <div className='cart'>
@@ -27,6 +45,7 @@ console.log(cart);
         }
 
       </div>
+      <button onClick={handlePurchases} style={{fontSize:'30px',cursor: 'pointer'}}>Buy Now</button>
     </div>
   )
 }
